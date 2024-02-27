40b150c879f07260df479fd52af4832773c357ef
Exter-dg <prkandmap23@rocketmail.com>
2024-02-27 15:01:19 +0530

    add code

--- abc.js
+++ abc.js
@@ -1,6 +1,15 @@
 console.log("Hello World")
 
+const newObj2 = {
+    name: "khushbu"
+}
+
 const obj = {
     abc: "parth",
+    newKey: "hi",
     bc: "parth"
+}
+
+const newObj = {
+    abc: "khushbu"
 }
